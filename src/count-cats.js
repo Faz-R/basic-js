969c4b7df22b9444de288f8244104323dd06c417
Faz-R <FazaR92@gmail.com>
2022-10-16 00:31:26 +0300

    feat: complete 4 task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats( matrix ) {
+  let res = 0;
+  matrix.forEach(e => e.forEach(elem => {if(elem == '^^'){res++}}))
+  return res;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {  
+  if(!Array.isArray(members)){
+    return false;
+  }
+  let res = [];
+  for (let word of members) {
+    if (typeof word == 'string') {
+      res.push(word.trim().toUpperCase().slice(0, 1))
+    }
+  }
+
+  return res.sort().join('');
 }
 
 module.exports = {
